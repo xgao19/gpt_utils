d6680d52874224a5bfd5a2ebfd686c1251c99bab
Philipp Scior <pscior@bnl.gov>
2022-04-08 14:02:12 -0400

    added qpdf and gluon helicity for proton

---
+++ gluon_helicity_rc.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+#
+# Authors: Christoph Lehner 2020 / Philipp Scior 2021
+#
+# Calculate proton QPDF with A2A method
+#
+import gpt as g
+import os
+from gpt_qpdf_utils import proton_qpdf_measurement
+
+# configure
+# root_output = "/p/project/chbi21/gpt_test/DA"
+root_output ="."
+
+# 420, 500, 580
+groups = {
+    "booster_batch_0": {
+        "confs": [
+            "420",
+            "1960",
+            "2000",
+        ],
+        #"evec_fmt": "/p/scratch/gm2dwf/evecs/96I/%s/lanczos.output",
+	    "evec_fmt": "/p/project/chbi21/gpt_test/96I/lanczos.output",
+        "conf_fmt": "/p/project/chbi21/gpt_test/96I/ckpoint_lat.%s",
+    },
+
+}
+parameters = {
+    "pf" : [1,1,0,0],
+    "q" : [0,1,0,0],
+    "zmax" : 4,
+    "t_insert" : 4,
+    "width" : 2.2,
+    "boost_in" : [0,0,0],
+    "save_propagators" : True
+}
+
+
+jobs = {
+    "booster_exact_0": {
+        "exact": 1,
+        "sloppy": 0,
+        "low": 0,
+    },  
+    "booster_sloppy_0": {
+        "exact": 0,
+        "sloppy": 10,
+        "low": 0,
+    }, 
+}
+
+
+jobs_per_run = g.default.get_int("--gpt_jobs", 1)
+
+
+
+# find jobs for this run
+def get_job(only_on_conf=None):
+    # statistics
+    n = 0
+    for group in groups:
+        for job in jobs:
+            for conf in groups[group]["confs"]:
+                n += 1
+
+    jid = -1
+    # for job in jobs:
+    #    for group in groups:
+    #        for conf in groups[group]["confs"]:
+    for group in groups:
+        for conf in groups[group]["confs"]:
+            for job in jobs:
+                jid += 1
+                if only_on_conf is not None and only_on_conf != conf:
+                    continue
+                root_job = f"{root_output}/{conf}/{job}"
+                if not os.path.exists(root_job):
+                    os.makedirs(root_job)
+                    return group, job, conf, jid, n
+
+    return None
+
+
+if g.rank() == 0:
+    first_job = get_job()
+    run_jobs = str(
+        list(
+            filter(
+                lambda x: x is not None,
+                [first_job] + [get_job(first_job[2]) for i in range(1, jobs_per_run)],
+            )
+        )
+    ).encode("utf-8")
+else:
+    run_jobs = bytes()
+run_jobs = eval(g.broadcast(0, run_jobs).decode("utf-8"))
+
+# every node now knows what to do
+
+
+
+# configuration needs to be the same for all jobs, so load eigenvectors and configuration
+conf = run_jobs[0][2]
+group = run_jobs[0][0]
+
+
+##### small dummy used for testing
+grid = g.grid([8,8,8,8], g.double)
+rng = g.random("seed text")
+U = g.qcd.gauge.random(grid, rng)
+
+# loading gauge configuration
+# U = g.load(groups[group]["conf_fmt"] % conf)
+g.message("finished loading gauge config")
+
+
+# do gauge fixing
+
+U_prime, trafo = g.gauge_fix(U, maxiter=500)
+del trafo
+
+L = U[0].grid.fdimensions
+
+Measurement = proton_qpdf_measurement(parameters)
+
+
+
+# show available memory
+g.mem_report(details=False)
+g.message(
+    """
+================================================================================
+       Gluon Helicity run on booster ;  this run will attempt:
+================================================================================
+"""
+)
+# per job
+for group, job, conf, jid, n in run_jobs:
+    g.message(
+        f"""
+
+    Job {jid} / {n} :  configuration {conf}, job tag {job}
+
+"""
+    )
+
+    job_seed = job.split("_correlated")[0]
+    rng = g.random(f"QPDF-ensemble-{conf}-{job_seed}")
+
+    source_positions_sloppy = [
+        [rng.uniform_int(min=0, max=L[i] - 1) for i in range(4)]
+        for j in range(jobs[job]["sloppy"])
+    ]
+    source_positions_exact = [
+        [rng.uniform_int(min=0, max=L[i] - 1) for i in range(4)]
+        for j in range(jobs[job]["exact"])
+    ]
+
+
+
+    g.message(f" positions_sloppy = {source_positions_sloppy}")
+    g.message(f" positions_exact = {source_positions_exact}")
+
+    root_job = f"{root_output}/{conf}/{job}"
+
+    Measurement.set_output_facilites(f"{root_job}/correlators",f"{root_job}/propagators")
+
+    g.message("Starting Wilson loops")
+    W = Measurement.create_WL(U_prime)
+
+    #Variant 1
+
+    Ex = g.field_strength(U_prime, 3, 0)
+    Ey = g.field_strength(U_prime, 3, 1)
+    Ez = g.field_strength(U_prime, 3, 2)
+
+    Ax = U_prime[0] - g.adj(U_prime[0]) + g.cshift(U_prime[0],0,-1) + g.adj(g.cshift(U_prime[0],0,-1))
+    Ax -= g.identity(Ax) * g.trace(Ax) / 3
+
+    Ay = U_prime[1] - g.adj(U_prime[1]) + g.cshift(U_prime[1],1,-1) + g.adj(g.cshift(U_prime[1],1,-1))
+    Ay -= g.identity(Ay) * g.trace(Ay) / 3
+
+    Az = U_prime[2] - g.adj(U_prime[2]) + g.cshift(U_prime[2],2,-1) + g.adj(g.cshift(U_prime[2],2,-1))
+    Az -= g.identity(Az) * g.trace(Az) / 3
+
+
+    Sg_x = g.slice(g.trace(Ey * Az - Ez * Ay), 3)
+    Sg_y = g.slice(g.trace(Ez * Ax - Ex * Az), 3)
+    Sg_z = g.slice(g.trace(Ex * Ay - Ey * Ax), 3)
+
+    #do output
+
+    #Variant 2
+
+    zmax = 20
+
+    dual_F3 = [] 
+    dual_F3[0] = 0.5 * (g.field_strength(U_prime, 2, 1) - g.field_strength(U_prime, 1, 2))
+    dual_F3[1] = 0.5 * (g.field_strength(U_prime, 0, 2) - g.field_strength(U_prime, 2, 0))
+    dual_F3[2] = 0.5 * (g.field_strength(U_prime, 1, 0) - g.field_strength(U_prime, 0, 1))
+
+    for z in range(zmax):
+
+        F3 = []
+        for i in range(3):
+            F3[i].append(g.field_strength(g.cshift(U_prime, 3, z), 3, i))
+
+        corr = g.slice(g.trace((F3[0]+F3[1]+F3[2])*W[z]*(dual_F3[0]+dual_F3[1]+dual_F3[2])),3)
+
+        #do output
+
+        
+
+        
+
+
+
+    
+
+
+
+    
+
+
+
+     
+
+
--- gpt_qpdf_utils.py
+++ gpt_qpdf_utils.py
@@ -7,6 +7,15 @@ import numpy as np
 #ordered list of gamma matrix identifiers, needed for the tag in the correlator output
 my_gammas = ["5", "T", "T5", "X", "X5", "Y", "Y5", "Z", "Z5", "I", "SXT", "SXY", "SXZ", "SYT", "SYZ", "SZT"]
 
+ordered_list_of_gammas = [g.gamma[5], g.gamma["T"], g.gamma["T"]*g.gamma[5],
+                                      g.gamma["X"], g.gamma["X"]*g.gamma[5], 
+                                      g.gamma["Y"], g.gamma["Y"]*g.gamma[5],
+                                      g.gamma["Z"], g.gamma["Z"]*g.gamma[5], 
+                                      g.gamma["I"], g.gamma["SigmaXT"], 
+                                      g.gamma["SigmaXY"], g.gamma["SigmaXZ"] 
+                                      g.gamma["SigmaZT"]
+                            ]
+
 class pion_measurement:
     def __init__(self, parameters):
         self.plist = parameters["plist"]
@@ -389,3 +398,238 @@ class pion_qpdf_measurement(pion_measurement):
         #This is now in principle B^dagger_zx but with the complex conj phase and a missing factor of gamma5 
 
         return (g.adj(dst_seq)*g.gamma[5])
+
+class proton_measurement:
+    def __init__(self, parameters):
+        self.plist = parameters["plist"]
+        self.pol_list = ["P+_Sz+","P+_Sx+","P+_Sx-"]
+        self.width = parameters["width"]
+        self.pos_boost = parameters["pos_boost"]
+
+     #make the inverters needed for the 96I lattices
+    def make_96I_inverter(self, U, evec_file):
+
+        l_exact = g.qcd.fermion.mobius(
+            U,
+            {
+                "mass": 0.00054,
+                "M5": 1.8,
+                "b": 1.5,
+                "c": 0.5,
+                "Ls": 12,
+                "boundary_phases": [1.0, 1.0, 1.0, -1.0],},
+        )
+
+        l_sloppy = l_exact.converted(g.single)
+
+        eig = g.load(evec_file, grids=l_sloppy.F_grid_eo)
+        # ## pin coarse eigenvectors to GPU memory
+        pin = g.pin(eig[1], g.accelerator)
+
+
+        light_innerL_inverter = g.algorithms.inverter.preconditioned(
+            g.qcd.fermion.preconditioner.eo1_ne(parity=g.odd),
+            g.algorithms.inverter.sequence(
+                g.algorithms.inverter.coarse_deflate(
+                    eig[1],
+                    eig[0],
+                    eig[2],
+                    block=400,
+                    fine_block=4,
+                    linear_combination_block=32,
+                ),
+                g.algorithms.inverter.split(
+                    g.algorithms.inverter.cg({"eps": 1e-8, "maxiter": 200}),
+                    mpi_split=g.default.get_ivec("--mpi_split", None, 4),
+                ),
+            ),
+        )
+
+        light_innerH_inverter = g.algorithms.inverter.preconditioned(
+            g.qcd.fermion.preconditioner.eo1_ne(parity=g.odd),
+            g.algorithms.inverter.sequence(
+                g.algorithms.inverter.coarse_deflate(
+                    eig[1],
+                    eig[0],
+                    eig[2],
+                    block=400,
+                    fine_block=4,
+                    linear_combination_block=32,
+                ),
+                g.algorithms.inverter.split(
+                    g.algorithms.inverter.cg({"eps": 1e-8, "maxiter": 300}),
+                    mpi_split=g.default.get_ivec("--mpi_split", None, 4),
+                ),
+            ),
+        )
+
+        light_exact_inverter = g.algorithms.inverter.defect_correcting(
+            g.algorithms.inverter.mixed_precision(light_innerH_inverter, g.single, g.double),
+            eps=1e-8,
+            maxiter=10,
+        )
+
+        light_sloppy_inverter = g.algorithms.inverter.defect_correcting(
+            g.algorithms.inverter.mixed_precision(light_innerL_inverter, g.single, g.double),
+            eps=1e-8,
+            maxiter=2,
+        )
+
+
+        ############### final inverter definitions
+        prop_l_sloppy = l_exact.propagator(light_sloppy_inverter).grouped(6)
+        prop_l_exact = l_exact.propagator(light_exact_inverter).grouped(6)
+
+        return prop_l_exact, prop_l_sloppy, pin
+
+    def make_debugging_inverter(self, U):
+
+        l_exact = g.qcd.fermion.mobius(
+            U,
+            {
+                "mass": 0.00054,
+                "M5": 1.8,
+                "b": 1.5,
+                "c": 0.5,
+                "Ls": 12,
+                "boundary_phases": [1.0, 1.0, 1.0, -1.0],},
+        )
+
+        l_sloppy = l_exact.converted(g.single)
+
+        light_innerL_inverter = g.algorithms.inverter.preconditioned(g.qcd.fermion.preconditioner.eo1_ne(parity=g.odd), g.algorithms.inverter.cg(eps = 1e-8, maxiter = 200))
+        light_innerH_inverter = g.algorithms.inverter.preconditioned(g.qcd.fermion.preconditioner.eo1_ne(parity=g.odd), g.algorithms.inverter.cg(eps = 1e-8, maxiter = 300))
+
+        light_exact_inverter = g.algorithms.inverter.defect_correcting(
+            g.algorithms.inverter.mixed_precision(light_innerH_inverter, g.single, g.double),
+            eps=1e-8,
+            maxiter=10,
+        )
+
+        light_sloppy_inverter = g.algorithms.inverter.defect_correcting(
+            g.algorithms.inverter.mixed_precision(light_innerL_inverter, g.single, g.double),
+            eps=1e-8,
+            maxiter=2,
+        )
+
+        prop_l_sloppy = l_exact.propagator(light_sloppy_inverter).grouped(6)
+        prop_l_exact = l_exact.propagator(light_exact_inverter).grouped(6)
+        return prop_l_exact, prop_l_sloppy
+
+
+    ############## make list of complex phases for momentum proj.
+    def make_mom_phases(self, grid):    
+        one = g.complex(grid)
+        pp = [-2 * np.pi * np.array(p) / grid.fdimensions for p in self.plist]
+        P = g.exp_ixp(pp)
+        mom = [g.eval(pp*one) for pp in P]
+        return mom
+
+    # create Wilson lines from all --> all + dz for all dz in 0,zmax
+    def create_WL(self, U):
+        W = []
+        W.append(g.qcd.gauge.unit(U[2].grid)[0])
+        for dz in range(0, self.zmax):
+            W.append(g.eval(W[dz-1] * g.cshift(U[2], 2, dz)))
+                
+        return W
+
+
+    #function that does the contractions for the smeared-smeared pion 2pt function
+    def contract_2pt(self, prop_f, phases, trafo, tag):
+
+        g.message("Begin sink smearing")
+        tmp_trafo = g.convert(trafo, prop_f.grid.precision)
+
+        prop_f = g.create.smear.boosted_smearing(tmp_trafo, prop_f, w=self.width, boost=self.pos_boost)
+        g.message("Sink smearing completed")
+
+        #This ans the IO still need work
+        corr = g.slice_proton(prop_f, phases, 3) 
+
+        #do correlator output 
+        corr_tag = "%s/2pt" % (tag)
+        for i, corr_pol in enumerate(corr):
+            out_tag = f"{corr_tag}/Pol{self.pol_list[i]}"
+            for j, corr_p in enumerate(corr_pol):
+                out_tag = f"{corr_tag}/p{self.plist[j]}"
+                self.output_correlator.write(out_tag, corr_p)
+                #g.message("Correlator %s\n" % out_tag, corr_t)
+
+    #function that creates boosted, smeared src.
+    def create_src(self, pos, trafo, grid):
+        
+        srcD = g.mspincolor(grid)
+        srcD[:] = 0
+        
+        g.create.point(srcD, pos)
+
+        srcDp = g.create.smear.boosted_smearing(trafo, srcD, w=self.width, boost=self.pos_boost)
+
+        del srcD
+
+        return srcDp     
+
+
+class proton_qpdf_measurement(proton_measurement):
+   
+    def __init__(self, parameters):
+        self.zmax = parameters["zmax"]
+        self.p = parameters["pf"]
+        self.q = parameters["q"]
+        self.plist = [self.q,]
+        self.pol_list = ["P+_Sz+","P+_Sx+","P+_Sx-"]
+        self.Gamma = parameters["gamma"]
+        self.t_insert = parameters["t_insert"]
+        self.width = parameters["width"]
+        self.boost_in = parameters["boost_in"]
+        self.boost_out = parameters["boost_out"]
+        self.pos_boost = self.boost_in
+
+    def create_fw_prop_QPDF(self, prop_f, W):
+        g.message("Creating list of W*prop_f for all z")
+        prop_list = [prop_f,]
+
+        for z in range(1,self.zmax):
+            prop_list.append(g.eval(W[z]*g.cshift(prop_f,2,z)))
+        
+        return prop_list  
+
+    def create_bw_seq(self, inverter, prop, trafo):
+        tmp_trafo = g.convert(trafo, prop.grid.precision)
+
+        #Make SS propagator
+        prop = g.create.smear.boosted_smearing(tmp_trafo, prop, w=self.width, boost=self.boost_out)
+
+        pp = 2.0 * np.pi * np.array(self.p) / prop.grid.fdimensions
+        P = g.exp_ixp(pp)
+
+        # sequential solve through t=insertion_time for all 3 proton polarizations
+        src_seq = [g.mspincolor(grid) for i in range(3)]
+        dst_seq = []
+        g.qcd.baryon.proton_seq_src(prop, src_seq, self.t_insert)
+
+        dst_tmp = g.lattice(src_seq)
+        for i in range(3):
+
+            dst_tmp @= inverter * g.create.smear.boosted_smearing(tmp_trafo, g.gamma[5]* P* g.conj(src_seq[i]), w=self.width, boost=self.boost_out)
+            del src_seq[i]
+            dst_seq.append(dst_tmp)
+
+        return dst_seq            
+
+
+    def contract_QPDF(self, prop_f, prop_bw, phases, tag):
+ 
+        #This and the IO still need work
+
+        for pol in self.pol_list:
+            corr = g.slice_trQPDF(g.gamma[5] *g.conj(prop_bw[i]), prop_f, phases, 3)
+
+            corr_tag = f"{tag}/QPDF/Pol{self.pol_list[pol]}"
+            for z, corr_p in enumerate(corr):
+                for i, corr_mu in enumerate(corr_p):
+                    p_tag = f"{corr_tag}/pf{self.p}/q{self.q}"
+                    for j, corr_t in enumerate(corr_mu):
+                        out_tag = f"{p_tag}/{my_gammas[j]}"
+                        self.output_correlator.write(out_tag, corr_t)
